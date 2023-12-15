e41b94306ca10bd1f1b761133a8f6cb0ba2a547c
primitive-god <maxwellesele@gmail.com>
2023-12-15 14:06:01 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- *main function that prints a message,with proper grammar, but the outcome is a piece of art
- *
+ *main - Prints "Programming is like building a multilingual
+ *puzzle, followed by a new line.
  *
  *
  *Return:Always 0.
  **/
 
-int main(void){
+int main(void)
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
