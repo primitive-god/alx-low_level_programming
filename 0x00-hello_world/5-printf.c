227530df35300b0e6e4ab5d46f0b993fa25a68ae
primitive-god <maxwellesele@gmail.com>
2023-12-15 14:02:38 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *This is a simple C program that prints a message.
- * The main function is the entry point of the program.
+ *main function that prints a message,with proper grammar, but the outcome is a piece of art
  *
  *
- *Return:Always (0)
+ *
+ *Return:Always 0.
  **/
 
-int main(void)
-{
+int main(void){
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
