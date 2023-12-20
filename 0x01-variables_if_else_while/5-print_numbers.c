917e35e9d0f3180fc22ca6c8d20b6035d0155ca1
primitive-god <maxwellesele@gmail.com>
2023-12-20 15:30:10 -0800

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	
 	int a;
-	for (a = 0; a < 10; a++)
-	{
-	
+	for (a = 0; a < 10; a++){
+		
 		printf("%d", a);
-		printf("\n");
+		
 	}
 		
-		return (0);
+	
+return (0);
 }
 
