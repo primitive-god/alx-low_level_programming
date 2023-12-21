8f9818a3e8b97b1792aff057ce51e31141a56574
primitive-god <maxwellesele@gmail.com>
2023-12-20 16:38:00 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*Main-prints the lowercase alphabet in reverse, followed by a new line.
+*
+*
+*Return:Always 0;
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+return (0);
+}
