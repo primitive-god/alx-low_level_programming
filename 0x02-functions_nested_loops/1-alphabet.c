07fa33f1deedef18406e0c96b99b3dae449f448b
primitive-god <maxwellesele@gmail.com>
2024-01-02 09:58:21 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main-prints the alphabet, in lowercase, followed by a new line.
+*
+*return: always (0)
+*/
+void print_alphabet(void)
+{	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	
+{
+	_putchar(c);
+	}
+	_putchar('\n');
+
+}
+
+
