8e7703dbbeb00d27a079d08bad8c780797acaf07
primitive-god <maxwellesele@gmail.com>
2024-01-02 08:13:46 -0800

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+/**
+*main -Write a program that prints _putchar, followed by a new line.
+*
+*
+*return: always (0)
+*/
+int _putchar(void)
+{	putchar(_putchar);
+	{
+		putchar("\n");
+
+return 0;
+	}
+}
+
