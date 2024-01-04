80a46ac195f9edc19be7f043fbc57f5b2429d640
primitive-god <maxwellesele@gmail.com>
2024-01-04 09:42:17 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,20 +3,21 @@
 /**
 *main- a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
 *
-*return the sum of the mutiples of 3 and 5 below 1024(excluded)
+*return: always 0
 */
 int main(void)
 {
 	int i = 0;
 	int sum = 0;
+
 		for (i = 0;i < 1024;i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
 	}
 
-	 printf("%d\n", sum);
-	 return 0;
+	printf("%d\n", sum);
+	 return (0);
 }
