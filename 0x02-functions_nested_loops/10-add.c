29c8a9239b23bae5c88c965bf181f82f11456cf7
primitive-god <maxwellesele@gmail.com>
2024-01-04 08:16:42 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*main-a function that adds two integers and returns the result.
+*
+*return result
+*/
+int add(int x, int y)
+{
+
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*main- prints the 9 times table, starting with 0.
+*
+*return 0
+*/
+void times_table(void)
+{
+	
+}
