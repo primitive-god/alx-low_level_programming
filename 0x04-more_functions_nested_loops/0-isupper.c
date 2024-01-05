471ab141f14f7389e47a438363b7950fcac3ded4
primitive-god <maxwellesele@gmail.com>
2024-01-05 06:17:55 -0800

    0-isup

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 *main-writes a function that checks for an uppercase letter
-*
-*return 1 if c is uppercase
+*@c is the number to be checked
+*return 1 if c is uppercase and 0 if otherwise
 */
 int _isupper(int c)
 {	
-	if (c >=65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{ 
 	return (1);
 	}
