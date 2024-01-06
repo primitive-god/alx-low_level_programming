6c05d343222977bb6edde146acdcd8641030c148
primitive-god <maxwellesele@gmail.com>
2024-01-06 07:20:15 -0800

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,18 +5,17 @@
 */
 void print_line(int n)
 {
-    int x;
-
-    if (n <= 0)  
-    {
+	int x;
+	if (n <= 0)  
+   	{
         _putchar('\n');
-    }
-    else
-    {
+    	}
+    	else
+    	{
         for (x = 0; x < n; x++)
         {
-            _putchar(95);
+	_putchar(95);
         }
         _putchar('\n');  
-    }
+	}
 }
