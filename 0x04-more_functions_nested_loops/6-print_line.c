72104c6b75d00c77a4aed083bffa1b98b4364537
primitive-god <maxwellesele@gmail.com>
2024-01-06 07:17:27 -0800

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*main-draws a straight line in the terminal.
+*return void
+*/
+void print_line(int n)
+{
+    int x;
+
+    if (n <= 0)  
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (x = 0; x < n; x++)
+        {
+            _putchar(95);
+        }
+        _putchar('\n');  
+    }
+}
