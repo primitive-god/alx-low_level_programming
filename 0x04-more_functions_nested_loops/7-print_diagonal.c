9b419050ab4d8f2ab2d1852e1ba84833117a8544
primitive-god <maxwellesele@gmail.com>
2024-01-06 08:06:54 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
 *main-draws a diagonal line in the terminal
-*
+*@n is number of time it should be drawn
 *return void
 */
+
 void print_diagonal(int n)
 {	
 	int i;
 	int j;
 	if (n <= 0)
-	{	_putchar('\n');
+	{	
+	_putchar('\n');
 	}
 	else
 	{
@@ -17,7 +20,7 @@ void print_diagonal(int n)
 	{
 	for (j = 0;j < n; j++)
 	{
-		_putchar(' ');
+	_putchar(' ');
 	}
 		
 	_putchar('\\');
