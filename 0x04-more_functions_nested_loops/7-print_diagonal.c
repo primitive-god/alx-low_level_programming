8f5e8e3fd1e13ff6716b80c1c96ceb204b7de512
primitive-god <maxwellesele@gmail.com>
2024-01-06 08:11:12 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,6 +10,7 @@ void print_diagonal(int n)
 {	
 	int i;
 	int j;
+
 	if (n <= 0)
 	{	
 	_putchar('\n');
@@ -20,10 +21,10 @@ void print_diagonal(int n)
 	{
 	for (j = 0;j < n; j++)
 	{
-	_putchar(' ');
+	_putchar(32);
 	}
 		
-	_putchar('\\');
+	_putchar(92);
 	_putchar('\n');
 	}
 	}
